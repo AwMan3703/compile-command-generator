3657595c22d8a22e51d52cb4ab20cb8cc67fe909
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2025-10-04 00:40:34 +0200

    dev

--- js/index.js
+++ js/index.js
@@ -1,19 +1,97 @@
+// CONSTANTS
+var SOURCE_CODE_EXTENSION = '.c';
+var COMPILER_INFO = {
+    output_path_parameter_name: '-o',
+    default_output_filename: 'a.out'
+};
 var commandOutputParagraph = document.querySelector('p#command-output');
+var copyCommandOutputButton = document.querySelector('button#copy-command-output');
 var commandBuilderOptionsForm = document.querySelector('form#command-builder-options');
+var commandBuilderInput_useCompiler = document.querySelector('form#command-builder-options input#use-compiler');
+var commandBuilderInput_compilerSelector = document.querySelector('form#command-builder-options select#compiler-selector');
+var commandBuilderInput_useSourcePath = document.querySelector('form#command-builder-options input#use-source-path');
+var commandBuilderInput_sourcePath = document.querySelector('form#command-builder-options input#source-path');
+var commandBuilderInput_outputPath = document.querySelector('form#command-builder-options input#output-path');
+var commandBuilderInput_runBinaryAfterCompiling = document.querySelector('form#command-builder-options input#run-binary-after-compiling');
+var commandBuilderInput_clearScreenBeforeRunning = document.querySelector('form#command-builder-options input#clear-screen-before-running');
+var commandBuilderInput_deleteBinaryAfterRunning = document.querySelector('form#command-builder-options input#delete-binary-after-running');
+// FUNCTIONS
 // EXAMPLE: clear && gcc my/source/code.c -o my/binary && ./my/binary && rm ./my/binary
 function generateCommand(options) {
-    var command;
-    return command;
+    var command = [];
+    // Compiler command
+    command.push("".concat(options.compilerName));
+    // if no source path is specified, return just this
+    if (!options.sourceCodePath)
+        return command.join(' ');
+    // Compiler options
+    if (options.sourceCodePath)
+        command.push("".concat(options.sourceCodePath));
+    if (options.binaryOutputPath)
+        command.push("".concat(COMPILER_INFO.output_path_parameter_name, " ").concat(options.binaryOutputPath));
+    command.push('&&');
+    // Run after compiling
+    if (options.runBinaryWhenCompiled) {
+        // Clear
+        if (options.clearScreenBeforeRunning) {
+            command.push("clear");
+            command.push('&&');
+        }
+        // Run
+        command.push("".concat(options.binaryOutputPath ? options.binaryOutputPath : COMPILER_INFO.default_output_filename));
+        command.push('&&');
+        // Delete
+        if (options.deleteBinaryAfterRunning) {
+            command.push("rm ".concat(options.binaryOutputPath ? options.binaryOutputPath : COMPILER_INFO.default_output_filename));
+            command.push('&&');
+        }
+    }
+    // Lazy aah solution cz ion wanna keep track of what the last command is
+    if (command[command.length - 1] === '&&')
+        command.pop();
+    return command.join(' ');
 }
-function updateCommand() {
-    var options = {
-        compiler: 'gcc',
-        sourceCodeFile: './mycode.c',
-        binaryOutputFile: './mybin',
-        runBinaryWhenCompiled: true,
-        clearScreenBeforeRunning: true,
-        deleteBinaryAfterRunning: true
-    };
+function getCommandBuilderFormData() {
+    var options = {};
+    options.compilerName = commandBuilderInput_useCompiler ? commandBuilderInput_compilerSelector.selectedOptions[0].value : '';
+    options.sourceCodePath = commandBuilderInput_useSourcePath.checked ? commandBuilderInput_sourcePath.value : '';
+    options.binaryOutputPath = commandBuilderInput_outputPath.value;
+    options.runBinaryWhenCompiled = commandBuilderInput_runBinaryAfterCompiling.checked;
+    options.clearScreenBeforeRunning = commandBuilderInput_clearScreenBeforeRunning.checked;
+    options.deleteBinaryAfterRunning = commandBuilderInput_deleteBinaryAfterRunning.checked;
+    return options;
+}
+function updateCommandOutput() {
+    var options = getCommandBuilderFormData();
     commandOutputParagraph.innerText = generateCommand(options);
 }
-commandBuilderOptionsForm.addEventListener('change', updateCommand);
+// SCRIPT
+copyCommandOutputButton.addEventListener('click', function (_) {
+    navigator.clipboard.writeText(commandOutputParagraph.innerText)
+        .then(function (text) {
+        var originalLabel = copyCommandOutputButton.innerText;
+        copyCommandOutputButton.innerText = commandOutputParagraph.innerText.length > 0 ? 'Copiato!' : 'Nulla da copiare!';
+        setTimeout(function (_) { return copyCommandOutputButton.innerText = originalLabel; }, 2000);
+    });
+});
+commandBuilderInput_sourcePath.addEventListener('input', function (_) {
+    var e = commandBuilderInput_sourcePath;
+    if (e.value.endsWith(SOURCE_CODE_EXTENSION))
+        return;
+    e.value += SOURCE_CODE_EXTENSION;
+    e.selectionStart = e.selectionEnd = e.value.length - 2;
+});
+commandBuilderInput_outputPath.addEventListener('input', function (_) {
+    var e = commandBuilderInput_outputPath;
+    if (e.value && e.value !== '')
+        e.value = (!e.value.startsWith('./') ? './' : '') + (e.value !== '.' ? e.value : '');
+    else
+        e.value = COMPILER_INFO.default_output_filename;
+});
+commandBuilderOptionsForm.addEventListener('change', updateCommandOutput);
+commandBuilderOptionsForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    updateCommandOutput();
+});
+updateCommandOutput();
+setInterval(function (_) { return commandOutputParagraph.classList.toggle('cursor'); }, 500);
--- js/index.ts
+++ js/index.ts
@@ -1,35 +1,115 @@
+// CONSTANTS
+const SOURCE_CODE_EXTENSION = '.c'
+const COMPILER_INFO = {
+    output_path_parameter_name: '-o',
+    default_output_filename: 'a.out'
+}
+
 const commandOutputParagraph = document.querySelector('p#command-output') as HTMLParagraphElement
+const copyCommandOutputButton = document.querySelector('button#copy-command-output') as HTMLButtonElement
+
 const commandBuilderOptionsForm = document.querySelector('form#command-builder-options') as HTMLFormElement
+const commandBuilderInput_useCompiler = document.querySelector('form#command-builder-options input#use-compiler') as HTMLInputElement
+const commandBuilderInput_compilerSelector = document.querySelector('form#command-builder-options select#compiler-selector') as HTMLSelectElement
+const commandBuilderInput_useSourcePath = document.querySelector('form#command-builder-options input#use-source-path') as HTMLInputElement
+const commandBuilderInput_sourcePath = document.querySelector('form#command-builder-options input#source-path') as HTMLInputElement
+const commandBuilderInput_outputPath = document.querySelector('form#command-builder-options input#output-path') as HTMLInputElement
+const commandBuilderInput_runBinaryAfterCompiling = document.querySelector('form#command-builder-options input#run-binary-after-compiling') as HTMLInputElement
+const commandBuilderInput_clearScreenBeforeRunning = document.querySelector('form#command-builder-options input#clear-screen-before-running') as HTMLInputElement
+const commandBuilderInput_deleteBinaryAfterRunning = document.querySelector('form#command-builder-options input#delete-binary-after-running') as HTMLInputElement
 
 type commandOptions = {
-    compiler: string // Will always be gcc
-    sourceCodeFile: string
-    binaryOutputFile: string
+    compilerName: string
+    sourceCodePath: string
+    binaryOutputPath: string
     runBinaryWhenCompiled: boolean
     clearScreenBeforeRunning: boolean
     deleteBinaryAfterRunning: boolean
 }
 
+
+// FUNCTIONS
+
 // EXAMPLE: clear && gcc my/source/code.c -o my/binary && ./my/binary && rm ./my/binary
 function generateCommand(options: commandOptions) {
-    let command: string
+    let command: string[] = []
 
-    command += `${}`
+    // Compiler command
+    command.push(`${options.compilerName}`)
+    // if no source path is specified, return just this
+    if (!options.sourceCodePath) return command.join(' ')
+    // Compiler options
+    if (options.sourceCodePath) command.push(`${options.sourceCodePath}`)
+    if (options.binaryOutputPath) command.push(`${COMPILER_INFO.output_path_parameter_name} ${options.binaryOutputPath}`)
+    command.push('&&')
+    // Run after compiling
+    if (options.runBinaryWhenCompiled) {
+        // Clear
+        if (options.clearScreenBeforeRunning) {
+            command.push(`clear`)
+            command.push('&&')
+        }
+        // Run
+        command.push(`${options.binaryOutputPath ? options.binaryOutputPath : COMPILER_INFO.default_output_filename}`)
+        command.push('&&')
+        // Delete
+        if (options.deleteBinaryAfterRunning) {
+            command.push(`rm ${options.binaryOutputPath ? options.binaryOutputPath : COMPILER_INFO.default_output_filename}`)
+            command.push('&&')
+        }
+    }
 
-    return command
+    // Lazy aah solution cz ion wanna keep track of what the last command is
+    if (command[command.length-1] === '&&') command.pop()
+
+    return command.join(' ')
 }
 
-function updateCommand() {
-    const options: commandOptions = {
-        compiler: 'gcc',
-        sourceCodeFile: './mycode.c',
-        binaryOutputFile: './mybin',
-        runBinaryWhenCompiled: true,
-        clearScreenBeforeRunning: true,
-        deleteBinaryAfterRunning: true
-    }
+function getCommandBuilderFormData() {
+    const options: commandOptions = {}
+    options.compilerName = commandBuilderInput_useCompiler ? commandBuilderInput_compilerSelector.selectedOptions[0].value : ''
+    options.sourceCodePath = commandBuilderInput_useSourcePath.checked ? commandBuilderInput_sourcePath.value : ''
+    options.binaryOutputPath = commandBuilderInput_outputPath.value
+    options.runBinaryWhenCompiled = commandBuilderInput_runBinaryAfterCompiling.checked
+    options.clearScreenBeforeRunning = commandBuilderInput_clearScreenBeforeRunning.checked
+    options.deleteBinaryAfterRunning = commandBuilderInput_deleteBinaryAfterRunning.checked
+    return options
+}
+
+function updateCommandOutput() {
+    const options = getCommandBuilderFormData()
     commandOutputParagraph.innerText = generateCommand(options)
 }
 
 
-commandBuilderOptionsForm.addEventListener('change', updateCommand)
+
+// SCRIPT
+
+copyCommandOutputButton.addEventListener('click', _ => {
+    navigator.clipboard.writeText(commandOutputParagraph.innerText)
+        .then(text => {
+            const originalLabel = copyCommandOutputButton.innerText
+            copyCommandOutputButton.innerText = commandOutputParagraph.innerText.length > 0 ? 'Copiato!' : 'Nulla da copiare!'
+            setTimeout(_ => copyCommandOutputButton.innerText = originalLabel, 2000)
+        })
+})
+
+commandBuilderInput_sourcePath.addEventListener('input', _ => {
+    const e = commandBuilderInput_sourcePath
+    if (e.value.endsWith(SOURCE_CODE_EXTENSION)) return
+    e.value += SOURCE_CODE_EXTENSION
+    e.selectionStart = e.selectionEnd = e.value.length - 2
+})
+commandBuilderInput_outputPath.addEventListener('input', _ => {
+    const e = commandBuilderInput_outputPath
+    if (e.value && e.value !== '') e.value = (!e.value.startsWith('./') ? './' : '') + (e.value !== '.' ? e.value : '')
+    else e.value = COMPILER_INFO.default_output_filename
+})
+commandBuilderOptionsForm.addEventListener('change', updateCommandOutput)
+commandBuilderOptionsForm.addEventListener('submit', e => {
+    e.preventDefault()
+    updateCommandOutput()
+})
+
+updateCommandOutput()
+setInterval(_ => commandOutputParagraph.classList.toggle('cursor'), 500)
